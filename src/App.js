99b3a4d026aa7d8b8302537c229eb8ad279394ef
sunil parmar <sunil04ce@gmail.com>
2023-02-10 22:11:08 +0530

    added UserList, displayed newly added user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevState) => {
+      const newUser = { id: Math.random().toString(), name: userName, age: userAge };
+      console.log(newUser);
+      return [...prevState, newUser];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div >
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -19,6 +19,17 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
+
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length == 0) {
+            return;
+        }
+
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        props.onAddUser(enteredUsername, enteredAge);
+
         console.log(enteredUsername, enteredAge);
 
         setEnteredUsername('');
